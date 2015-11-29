988a0923c1e86700be9696bb2465e22096c18521
yulingtianxia <yulingtianxia@gmail.com>
2015-11-29 22:45:42 +0800

    开机自启动

--- TFSHelper/AppDelegate.swift
+++ TFSHelper/AppDelegate.swift
@@ -7,17 +7,40 @@
 //
 
 import Cocoa
+import ServiceManagement
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
-    
+    let launcherAppIdentifier = "com.yulingtianxia.TFSHelperLauncher"
     let statusItem: NSStatusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
     let menu: NSMenu = NSMenu()
     var previousChangeCount: Int = 0
-    var autoCatch: Bool = true
+    var autoCatch: Bool = true {
+        didSet {
+            if autoCatch {
+                switchAutoCatchItem.title = "自动连接开启中"
+            }
+            else {
+                switchAutoCatchItem.title = "自动连接已关闭"
+            }
+        }
+    }
+    var autoLaunch: Bool = true {
+        didSet {
+            if autoLaunch {
+                switchAutoLaunchItem.title = "登录时启动"
+            }
+            else {
+                switchAutoLaunchItem.title = "登录时不启动"
+            }
+            SMLoginItemSetEnabled(launcherAppIdentifier, autoLaunch)
+        }
+    }
     let openLocationItem = NSMenuItem(title: "打开链接", action: "connect:", keyEquivalent: "")
     let quitItem = NSMenuItem(title: "退出", action: "terminate", keyEquivalent: "")
     let switchAutoCatchItem = NSMenuItem(title: "自动连接开启中", action: "switchAutoCatch", keyEquivalent: "")
+    let switchAutoLaunchItem = NSMenuItem(title: "登录时启动", action: "switchAutoLaunch", keyEquivalent: "")
+    let userDefaults = NSUserDefaults.standardUserDefaults()
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
@@ -25,16 +48,36 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
         menu.delegate = self
         menu.addItem(openLocationItem)
         menu.addItem(switchAutoCatchItem)
+        menu.addItem(switchAutoLaunchItem)
         menu.addItem(quitItem)
         
         statusItem.button?.image = NSImage(named: "TFSmenu")
         statusItem.menu = menu
         
+        userDefaults.registerDefaults(["autoCatch":autoCatch])
+        userDefaults.registerDefaults(["autoLaunch":autoLaunch])
+        
+        autoCatch = userDefaults.boolForKey("autoCatch")
+        autoLaunch = userDefaults.boolForKey("autoLaunch")
+        
         NSTimer.scheduledTimerWithTimeInterval(0.25, target: self, selector: "pollPasteboard:", userInfo: nil, repeats: true)
+        
+        var startedAtLogin = false
+        for app in NSWorkspace.sharedWorkspace().runningApplications {
+            if app.bundleIdentifier == launcherAppIdentifier {
+                startedAtLogin = true
+            }
+        }
+        
+        if startedAtLogin {
+            NSDistributedNotificationCenter.defaultCenter().postNotificationName("killme", object: NSBundle.mainBundle().bundleIdentifier!)
+        }
     }
     
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+        userDefaults.setBool(autoCatch, forKey: "autoCatch")
+        userDefaults.setBool(autoLaunch, forKey: "autoLaunch")
     }
     
     func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
@@ -59,18 +102,15 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     }
     
     func switchAutoCatch() {
-        if autoCatch {
-            autoCatch = false
-            switchAutoCatchItem.title = "自动连接已关闭"
-        }
-        else {
-            autoCatch = true
-            switchAutoCatchItem.title = "自动连接开启中"
-        }
+        autoCatch = !autoCatch
+    }
+    
+    func switchAutoLaunch() {
+        autoLaunch = !autoLaunch
     }
     
     func terminate() {
-        NSApplication.sharedApplication().terminate(nil)
+        NSApp.terminate(nil)
     }
     
     // MARK: NSMenuDelegate
---
+++ TFSHelperLauncher/AppDelegate.swift
@@ -0,0 +1,57 @@
+//
+//  AppDelegate.swift
+//  TFSHelperLauncher
+//
+//  Created by 杨萧玉 on 15/11/29.
+//  Copyright © 2015年 杨萧玉. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    let mainAppIdentifier = "com.yulingtianxia.TFSHelper"
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+        
+        let running = NSWorkspace.sharedWorkspace().runningApplications
+        var alreadyRunning = false
+        
+        for app in running {
+            if app.bundleIdentifier == mainAppIdentifier {
+                alreadyRunning = true
+                break
+            }
+        }
+        
+        if !alreadyRunning {
+            NSDistributedNotificationCenter.defaultCenter().addObserver(self, selector: "terminate", name: "killme", object: mainAppIdentifier)
+            
+            let path = NSBundle.mainBundle().bundlePath as NSString
+            var components = path.pathComponents
+            components.removeLast()
+            components.removeLast()
+            components.removeLast()
+            components.append("MacOS")
+            components.append("TFSHelper") //main app name
+            
+            let newPath = NSString.pathWithComponents(components)
+            
+            NSWorkspace.sharedWorkspace().launchApplication(newPath)
+        }
+        else {
+            self.terminate()
+        }
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+    func terminate() {
+        NSApp.terminate(nil)
+    }
+}
+
